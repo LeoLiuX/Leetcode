b4bb561c7db6e9d483b0f02d0cdbb3bec29902eb
DESKTOP-VM6312P\liuxiao232 <liuxiao232@gmail.com>
2018-08-19 21:04:02 -0700

    Q35 SOLUTION

--- LeetcodeSolutions/LeetcodeSolutions/35.SearchInsertPosition.cpp
+++ LeetcodeSolutions/LeetcodeSolutions/35.SearchInsertPosition.cpp
@@ -13,12 +13,43 @@ class Solution
 {
 public:
 	int searchInsert(vector<int>& nums, int target) {
-		return 0;
+		int head = 0;
+		int tail = nums.size();
+		int mid = (head + tail) / 2;
+
+		while (true) {
+			if (nums[mid] == target || head >= tail) {
+				return mid;
+			}
+			if (nums[mid] < target) {
+				if (head == mid) {
+					return mid + 1;
+				}
+				head = mid;
+				mid = (head + tail) / 2;
+			}
+			else {
+				tail = mid;
+				mid = (head + tail) / 2;
+			}
+		}
+
+		// O(n) deprecated answer.
+		/*for (int i = 0; i < nums.size(); ++i) {
+			if (nums[i] == target || nums[i] > target){
+				return i;
+			}
+		}
+		return (int)nums.size();*/
 	}
 };
 
 #ifdef Q35
 int main() {
-	return 0;
+	Solution s;
+	vector<int> nums = { 1,3,5,6 };
+	int result = s.searchInsert(nums, 10);
+	CSharedHelper::PrintResult(result);
+	getchar();
 }
 #endif
